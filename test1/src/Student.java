6f1c5d1897cb94a830c9d761e8fee4591a30c80c
foresec <foresec@naver.com>
2023-07-04 00:06:05 +0900

    0704

---
+++ test1/src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    private int studentId;
+    private String studentName;
+
+    public Student(int studentId, String studentName)
+    {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+}
