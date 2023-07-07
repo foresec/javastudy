f946e9c9bc712f6281d918432d95d117064abfd6
foresec <foresec@naver.com>
2023-07-07 23:05:49 +0900

    0707

--- test1/src/tested.java
+++ test1/src/tested.java
@@ -2,6 +2,6 @@ public class tested {
     public static void main(String[] args) {
         int num = 5;
         System.out.println("!");
-        System.out.printf("%s",num);
+        System.out.printf("%s d",num);
     }
 }
