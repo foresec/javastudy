8d8edc57add565cd9ccc417d756cf3272abb9ad8
foresec <foresec@naver.com>
2023-08-16 14:57:56 +0900

    0816

--- spring/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ spring/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -9,9 +10,39 @@ public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-        EntityManager entityManager = emf.createEntityManager();
+        EntityManager em = emf.createEntityManager();
 
-        entityManager.close();
+        // 트랜젝션 필요
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member member = new Member();
+//
+//            member.setId(1L);
+//            member.setName("hello");
+//            em.persist(member);
+
+
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember = " + findMember.getId());
+            // 삭제
+//            em.remove(findMember);
+            // 수정 : 뭔가 바뀌면 JPA에서 알아서 DB에도 업데이트 해줌
+//            findMember.setName("HelloJPA");
+
+
+            tx.commit();
+
+            // JPQL :  DB테이블이 아닌 객체나 엔티티를 대상으로 하는 객체 지향 쿼리
+        } catch (Exception e) {
+            tx.rollback();
+
+        } finally {
+            // EntityManager는 사용이 다끝나면 닫아줘야함
+            em.close();
+            
+        }
 
 
         emf.close();
---
+++ spring/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+// JPA를 사용한다고 지정하기 위해 @Entity 사용
+@Entity
+// 아래와 같은 형식으로 테이블을 지정 가능.
+// @Table(name = "USER")
+public class Member {
+
+    // @Column(name = "username")
+    // 컬럼도 마찬가지
+    @Id
+    private Long id;
+    @Column(name = "name")
+    private String username;
+    private Integer age;
+    // @Enumerated : 보통 db에서 enum type을 못쓰므로 지정하는 어노테이션
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
+
+    // @Transient는 DB에 반영하지 않고 메모리안에서만 실행
+    @Transient
+    private int temp;
+    public Member() {
+    }
+
+
+
+}
